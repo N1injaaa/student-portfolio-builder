4a35bcac0ed4f5680c0f73843b9b17a427238a26
Nurgeldi Berkinbaev <berkinbaevnurgeldi@gmail.com>
2026-08-30 18:46:18 +0500

    Remove duplicate nested project folder

--- student-portfolio-builder/hooks/use-hydrated-profile.ts
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useProfileStore } from "@/lib/store";
-
-export function useHydratedProfile() {
-  const hasHydrated = useProfileStore((s) => s.hasHydrated);
-  const profile = useProfileStore((s) => s.profile);
-  return { hasHydrated, profile };
-}
